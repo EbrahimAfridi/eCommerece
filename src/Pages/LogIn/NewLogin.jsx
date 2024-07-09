08b71d848b9658f7f5e0501d79f629b174163010
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-07-09 12:19:29 +0530

    login screen new ui

--- src/Pages/LogIn/NewLogin.jsx
+++ src/Pages/LogIn/NewLogin.jsx
@@ -2,6 +2,7 @@ import { useFirebase } from "../../auth/FirebaseContext.jsx";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import "./NewLogin.css";
+import { GoogleLogo } from "phosphor-react";
 
 const NewLogin = () => {
   const { createUser, signInWithGoogle, signIn } = useFirebase();
@@ -13,8 +14,6 @@ const NewLogin = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError("");
-
     const buttonName = e.nativeEvent.submitter.name; // Get the name of the clicked button
 
     try {
@@ -39,57 +38,65 @@ const NewLogin = () => {
 
   return (
     <div className="flex">
-      {/*bg-[#3a3c19] bg-[#fceadb]*/}
-      <div className="hidden md:flex flex-col justify-center pl-5 w-[50vw] bg-black text-white h-screen">
-        <p className="italic-login text-2xl italic font-serif leading-[5rem]">
+      <div className="hidden md:flex flex-col items-start justify-center pl-5 w-[50vw] bg-black text-white h-screen">
+        <p className="italic-login text-md lg:text-2xl italic font-serif mb-4">
           Log in
         </p>
-        <p className="medium-welcome text-[8vw] font-bold md:leading-[5rem] leading-[4rem]">
-          WELCOME
-        </p>
-        <p className="medium-welcome text-[8vw] font-bold md:leading-[6rem] leading-[2rem]">
-          BACK
-        </p>
+        <div>
+          <p className="medium-welcome text-7xl lg:text-9xl font-bold">
+            WELCOME
+          </p>
+          <p className="medium-welcome text-7xl lg:text-9xl font-bold -mt-2 lg:-mt-4">
+            BACK
+          </p>
+        </div>
       </div>
 
-      <div className="flex justify-center items-center md:w-[50vw] w-[100%] bg-slate-50 text-black h-screen relative">
+      <div className="flex justify-center items-center md:w-[50vw] w-full bg-slate-50 text-black h-screen relative">
         <form
           className="flex flex-col items-center h-4/5 "
           onSubmit={handleSubmit}
         >
-          <h2 className="heading mb-8">Authentication</h2>
+          <h2 className="font-semibold mb-16 font-sans text-3xl lg:text-5xl tracking-wide">
+            Authentication
+          </h2>
+          {error && <div className="text-red-500 text-sm mb-2">{error}</div>}
           <div className="input-container">
-            <label className="label block text-xl font-bold mb-2">EMAIL</label>
+            <label className="block text-md lg:text-lg font-medium mb-2">
+              EMAIL
+            </label>
             <input
-              className="w-[75vw] md:w-96 h-12 mb-4 focus:border-blue-500 pl-2 bg-transparent border-solid rounded-[5px] border-black border-2 focus:outline-none"
+              className="w-[75vw] md:w-96 h-12 mb-6 focus:border-blue-600 pl-2 rounded-lg border border-zinc-300 shadow-sm focus:outline-none text-zinc-400 text-md font-light"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               type="email"
+              required
             />
           </div>
           <div className="input-container">
-            <label className="label block text-xl font-bold mb-2">
+            <label className="block text-md lg:text-lg font-medium mb-2">
               PASSWORD
             </label>
 
             <input
-              className="w-[75vw] md:w-96 h-12 mb-4 focus:border-blue-500 pl-2 bg-transparent border-solid rounded-[5px] border-black border-2 focus:outline-none"
+              className="w-[75vw] md:w-96 h-12 mb-10 focus:border-blue-600 pl-2 rounded-lg border border-zinc-300 shadow-sm focus:outline-none text-zinc-400 text-md font-light"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               type="password"
+              required
             />
           </div>
-          <div className="button-container flex items-center gap-5 mt-4">
-            <button
+          <div className="button-container flex items-center gap-5 mt-4 w-full">
+            {/* <button
               className="label text-white sign-up p-3 rounded-sm bg-stone-900"
               type="submit"
               name="signUp"
             >
               Sign Up
             </button>
-            <span>OR</span>
+            <span>OR</span> */}
             <button
-              className="label sign-in p-3 rounded-sm bg-stone-900 text-white"
+              className="sign-in p-3 bg-stone-900 text-white w-full rounded-lg border shadow-sm font-extralight"
               type="submit"
               name="signIn"
             >
@@ -98,13 +105,15 @@ const NewLogin = () => {
           </div>
 
           <button
-            className="label p-2 rounded-sm bg-stone-900 text-white mt-20 w-[75vw] md:w-96"
+            className="sign-in p-3 bg-white text-black w-full rounded-lg border shadow-sm font-extralight mt-4"
             onClick={handleGoogleSignIn}
             type="button"
             name="signInWithGoogle"
           >
-            {/*<FaGoogle className="google-icon" />*/}
-            Sign In with Google
+            <div className="flex items-center justify-center">
+              Sign In with
+              <GoogleLogo className="ml-2" size={20} />
+            </div>
           </button>
         </form>
       </div>
