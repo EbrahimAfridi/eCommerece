ec5f6e63f86b711199be121e045a210d30891b99
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-07-09 15:19:41 +0530

    fixed: wishlist ui

--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -1,96 +1,96 @@
 import { useContext, useEffect } from "react";
 import { ShopContext } from "../../Context/shop-context.jsx";
 import { useNavigate } from "react-router-dom";
-import './Wishlist.css';
-import {useFirebase} from "../../auth/FirebaseContext.jsx";
+import "./Wishlist.css";
+import { useFirebase } from "../../auth/FirebaseContext.jsx";
 import Navbar from "../../components/navbar.jsx";
 
 export const Wishlist = () => {
-    const {user} = useFirebase();
-    const navigate = useNavigate();
+  const { user } = useFirebase();
+  const navigate = useNavigate();
 
-    const {
-        wishlistItems,
-        updateTotalCartItemAmount,
-        cartItems,
-        removeFromWishlist,
-        selectedSize,  //to understand read export vars of context
-        addToCart,
-        products,
-    } = useContext(ShopContext);
-    
-    const handleProductClick = (productId) => {
-        navigate(`/productPage/${productId}`);
-    };
-      
-      
-    const handleRemoveFromWishlist = (productId) => {
-        removeFromWishlist(productId);
-    };
+  const {
+    wishlistItems,
+    updateTotalCartItemAmount,
+    cartItems,
+    removeFromWishlist,
+    selectedSize, //to understand read export vars of context
+    addToCart,
+    products,
+  } = useContext(ShopContext);
 
-    const handleAddToCart = (productId) => {
-        addToCart(productId);
-    };
+  const handleProductClick = (productId) => {
+    navigate(`/productPage/${productId}`);
+  };
 
-    useEffect(() => {
-        updateTotalCartItemAmount();
-    }, [cartItems, updateTotalCartItemAmount]);
+  const handleRemoveFromWishlist = (productId) => {
+    removeFromWishlist(productId);
+  };
 
-    return (
-        <div>
-            <Navbar/>
-            <div className="px-8 wishlist">
-            <h1 className="heading-h1 text-2xl text-center">Wishlist</h1>
-                <div className=" wishlistItems">
-                    {products.map((product) => {
-                        const quantity = wishlistItems[product.id] || 0;
-                        if (quantity > 0) {
-                            return (
-                                <div key={product.id} className="wishlistItem">
-                                    <img
-                                        src={product.image}
-                                        alt={product.title}
-                                        onClick={() => handleProductClick(product.id)}
-                                    />
-                                    <div className="wishlistItemDetails">
-                                        <p>{product.title}</p>
-                                        <p>{selectedSize(product.id)}</p>
-                                        <p>${product.price}</p>
-                                        <div className="addRemoveBtn">
-                                            <p
-                                                className="inline-block mr-4 add"
-                                                onClick={() => {
-                                                    handleAddToCart(product.id);
-                                                    handleRemoveFromWishlist(product.id);
-                                                }}
-                                            >
-                                                Add
-                                            </p>
-                                            <p
-                                                className="inline-block remove"
-                                                onClick={() => handleRemoveFromWishlist(product.id)}
-                                            >
-                                                Remove
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        }
-                        return null;
-                    })}
+  const handleAddToCart = (productId) => {
+    addToCart(productId);
+  };
+
+  useEffect(() => {
+    updateTotalCartItemAmount();
+  }, [cartItems, updateTotalCartItemAmount]);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="px-8 wishlist text-center">
+        <h1 className="heading-h1 text-4xl">Wishlist</h1>
+        <div className="wishlistItems">
+          {products.map((product) => {
+            const quantity = wishlistItems[product.id] || 0;
+            if (quantity > 0) {
+              return (
+                <div key={product.id} className="wishlistItem">
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    onClick={() => handleProductClick(product.id)}
+                  />
+                  <div className="wishlistItemDetails">
+                    <p>{product.title}</p>
+                    <p>{selectedSize(product.id)}</p>
+                    <p>${product.price}</p>
+                    <div className="addRemoveBtn">
+                      <p
+                        className="inline-block mr-4 add"
+                        onClick={() => {
+                          handleAddToCart(product.id);
+                          handleRemoveFromWishlist(product.id);
+                        }}
+                      >
+                        Add
+                      </p>
+                      <p
+                        className="inline-block remove"
+                        onClick={() => handleRemoveFromWishlist(product.id)}
+                      >
+                        Remove
+                      </p>
+                    </div>
+                  </div>
                 </div>
-            </div>
-            <div className="w-64 m-auto">
-                <button className="p-2 md:ml-10 text-lg text-white bg-black rounded-md"
-                        onClick={() => navigate("/shop")}
-                >
-                    Continue Shopping
-                </button>
-            </div>
-            
+              );
+            }
+            return null;
+          })}
         </div>
-    );
+      </div>
+      <div className="w-64 m-auto mt-4 flex justify-center">
+        <button
+          //   className="p-2 md:ml-10 text-lg text-white bg-black rounded-md"
+          className="rounded-md text-xs sm:text-lg outline-none bg-black text-white shadow-sm border p-2 tracking-wider hover:opacity-70"
+          onClick={() => navigate("/shop")}
+        >
+          Continue Shopping
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Wishlist;
