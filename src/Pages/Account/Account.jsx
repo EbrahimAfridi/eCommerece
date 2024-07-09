efde8a0320ee61ac66f22db80b48f7aeac26a9fd
ebrahim afridi <afridiebr@student.iul.ac.in>
2024-07-09 16:33:27 +0530

    fixed: account page ui

--- src/Pages/Account/Account.jsx
+++ src/Pages/Account/Account.jsx
@@ -1,11 +1,12 @@
-import {useFirebase} from "../../auth/FirebaseContext.jsx";
-import {useNavigate} from "react-router-dom";
+import { useFirebase } from "../../auth/FirebaseContext.jsx";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/navbar.jsx";
-import {User} from "phosphor-react";
+import { User } from "phosphor-react";
 
 const Account = () => {
+  const { user, logout } = useFirebase();
+  console.log(user);
 
-  const {user, logout} = useFirebase();
   const navigate = useNavigate();
 
   const handleLogOut = async () => {
@@ -15,45 +16,38 @@ const Account = () => {
     } catch (e) {
       console.error(e.message);
     }
-  }
+  };
 
-  return(
-    <>
-
-      <Navbar/>
-
-    <div className="container absolute top-[20%] left-[35%]
-    bg-black text-white w-[500px] h-[300px] p-10 rounded-xl"
-    >
-
-      <h1 className="text-[40px] flex justify-start items-center gap-3">
-        <User size={40} color="#f1efef"  />
-        Profile
-      </h1>
-
-      <div className="flex-col gap-10 justify-between items-end">
-
-        {
-          user.displayName && <p className="text-[20px] mt-4">Name: {user.displayName}</p>
-        }
-
-        <p className="text-[20px] mt-4">Email: {user.email}</p>
-
-        <button
-          onClick={handleLogOut}
-          className="label ml-40 mt-8 sign-in p-3 rounded-sm bg-white text-black"
-          type="submit"
-          name="signIn"
-        >
-          Log out
-        </button>
-
+  return (
+    <section className="flex flex-col items-center w-full h-screen">
+      <Navbar />
+      <div className="mt-40 w-full flex justify-center">
+        <div className="container shadow-lg bg-white text-zinc-900 p-8 w-96 rounded-md font-sans">
+          <div className="flex flex-col items-center gap-4">
+            <div className="w-24 h-24 rounded-full overflow-hidden">
+              <img
+                src={user.photoURL ? user.photoURL : "/vite.svg"}
+                alt="profile picture"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-lg font-semibold">
+              {user.displayName ? user.displayName : "test-user"}
+            </p>
+            <p className="text-lg">{user.email ? user.email : "example@email.com"}</p>
+            <button
+              onClick={handleLogOut}
+              className="mt-8 p-3 rounded bg-black text-white w-full hover:opacity-90"
+              type="submit"
+              name="signIn"
+            >
+              Log out
+            </button>
+          </div>
+        </div>
       </div>
-
-    </div>
-    </>
+    </section>
   );
-
 };
 
 export default Account;
