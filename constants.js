a86be99fd03280cb3e5e51f10335b5fe3f6d4598
ebrahim afridi <afridiebrahimck@gmail.com>
2025-03-09 02:12:55 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 export const VIDEO_GRID_URLS = [
   {
     id: 1,
-    videoURL: "https://i.imgur.com/yB1JD3N.mp4"
+    videoURL: "https://i.pinimg.com/736x/6b/d5/c2/6bd5c2c12c5defcf76a82784889e8e67.jpg"
   },
   {
     id: 2,
@@ -9,10 +9,10 @@ export const VIDEO_GRID_URLS = [
   },
   {
     id: 3,
-    videoURL: "https://i.imgur.com/58Kq4WQ.mp4"
+    videoURL: "https://i.pinimg.com/736x/41/d3/c4/41d3c46764ad1984ec8321e387feb4f2.jpg"
   },
   {
     id: 4,
-    videoURL: "https://i.imgur.com/KlUEZY0.mp4"
+    videoURL: "https://i.pinimg.com/736x/96/75/3f/96753ff5af0d0748968f0846e61163a2.jpg"
   }
 ];
